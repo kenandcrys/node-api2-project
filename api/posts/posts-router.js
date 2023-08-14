342ece1bdc9ec8064796d244dc07bbaad04f7481
Kenneth Fisher <kenandcrys@live.com>
2023-08-14 10:30:16 -0500

    Post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -30,6 +30,42 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({
       message: "The post information could not be retrieved",
     });
+  };
+});
+
+router.post('/',  (req, res) => {
+  const { title, contents } = req.body;
+  if(!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post"
+    })
+  } else {
+    Posts.insert({ title, contents })
+      .then(({ id }) => {
+        return Posts.findById(id)
+      })
+      .then(post => {
+        res.status(201).json(post)
+      })
+      .catch(err => {
+        res.status(500).json({ 
+          message: "There was an error while saving the post to the database" 
+        })
+      })
   }
 });
+
+
+router.delete('/:id', async (req, res) => {
+
+})
+
+router.put('/id', async (req, res) => {
+
+})
+
+router.get('/:id/message', async (req, res) => {
+
+})
+
 module.exports = router;
