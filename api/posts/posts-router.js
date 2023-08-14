36cc431c13fbb72778e7e34776cec0af8894f6e1
Kenneth Fisher <kenandcrys@live.com>
2023-08-14 16:35:56 -0500

    finished product

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,7 +5,6 @@ const Posts = require('./posts-model');
 
 const router = express.Router();
 
-// GET all posts
 router.get('/', async (_req, res) => {
   try {
     const posts = await Posts.find();
@@ -30,7 +29,7 @@ router.get('/:id', async (req, res) => {
     res.status(500).json({
       message: "The post information could not be retrieved",
     });
-  };
+  }
 });
 
 router.post('/', async (req, res) => {
@@ -73,13 +72,59 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/:id', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({ message: "Please provide title and contents for the post" })
+  } else {
+    Posts.findById(req.params.id)
+      .then( stuff => {
+        if(!stuff){
+          res.status(404).json({
+            message: "The post with the specified ID does not exist"
+          })
+        }else {
+          return Posts.update(req.params.id, req.body)
+        }
+      })
+      .then(data => {
+        if (data) {
+          return Posts.findById(req.params.id)
+        }
+      })
+      .then(post => {
+        res.json(post)
+      })
+      .catch( err => {
+        res.status(500).json({ 
+          message: "There was an error while saving the post to the database" ,
+          err: err.message,
+          stack: err.stack
+        })
+      })
+  }
+  
+});
 
+router.get('/:id/comments', async (req, res) => {
+        try {
+          const post = await Posts.findById(req.params.id);
+          if (!post) {
+            res.status(404).json({
+              message: `message: "The post with the specified ID does not exist"`
+            })
+          } else {
+            const messages = await Posts.findPostComments(req.params.id)
+            res.json(messages)
+          }
 
-router.put('/id', async (req, res) => {
-
-})
-
-router.get('/:id/message', async (req, res) => {
+        } catch (err) {
+          res.status(500).json({
+            message: "The comments information could not be retrieved",
+            err: err.message,
+            stack: err.stack,
+          })
+        }
 
 })
 
